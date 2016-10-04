1475d055f06a02d988a0f565ff02afb4048a01ac
flowstyleliving <mys628@gmail.com>
2016-10-03 21:46:06 -0700

    changed initial notes

--- client/client.js
+++ client/client.js
@@ -7,26 +7,23 @@ import { Provider } from 'react-redux'
 let initialState = {
   tasks: [{
     id: 0,
-    title: 'Simplify your Life!',
-    completed: false,
-    notes: [{
-        id: 0,
-        title: 'Be mindful of your notes',
-        completed: false
-    }]
-  }, {
-    id: 1,
     title: 'Have at most 3 Main Tasks a day!',
     completed: false,
     notes: [{
       id: 0,
       title: 'Cross them out with the blue-button',
       completed: false
-    }, {
-      id: 1,
-      title: 'Take a screenshot at the end of day!',
-      completed: false
     }]
+  },
+    {
+      id: 1,
+      title: 'Simplify your Life!',
+      completed: false,
+      notes: [{
+          id: 0,
+          title: 'Take a screenshot at the end of day!',
+          completed: false
+      }]
     }
   ],
   user: {
